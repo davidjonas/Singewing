c25d551bdd2759e596c0a229ed76dc81bc9a34dd
David Jonas <davidjonasdesign@gmail.com>
2017-11-19 18:03:31 +0100

    Initial server configuration, realtime user management and delay calculation

---
+++ js/singewing.js
@@ -0,0 +1,90 @@
+
+function UID() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+var Singewing = function () {
+  var self = this;
+  this.socket = io();
+  this.pingTime = 0;
+  this.delay = 0;
+  this.connected = false;
+  this.registered = false;
+  this.users = [];
+  this.name;
+
+  // RX - connection
+  this.socket.on('connection', function(){
+      self.connected = true;
+      self.updateUserList();
+  });
+
+  //RX - test ACK
+  this.socket.on("ack", function () {
+    alert("Test successful, message sent and received from the server.");
+  });
+
+  // RX -  pong
+  this.socket.on('sw_pong', function (time){
+    var now = new Date().getTime();
+    self.delay = now - self.pingTime;
+    console.log("pong    ==>    timeToServer: " + (time - self.pingTime) + "ms    ==>      full delay = " + self.delay + "ms");
+  });
+
+  //RX - new user
+  this.socket.on("newUser", function (user) {
+    console.log("NEW USER!!");
+    self.users.push(user);
+    if(user["name"] == self.name)
+    {
+      self.registered = true;
+    }
+  });
+
+  //RX - registration error
+  this.socket.on("registrationError", function (msg) {
+    console.log(msg);
+  });
+
+  //RX - Update user list Receive
+  this.socket.on("updateUserList", function (users) {
+    self.users = users;
+  });
+};
+
+//TX - ping
+Singewing.prototype.ping = function () {
+  console.log("ping");
+  this.pingTime = new Date().getTime();
+  this.socket.emit("sw_ping", singewing.pingTime);
+}
+
+//TX - test
+Singewing.prototype.test = function() {
+  this.socket.emit("test");
+};
+
+//TX - register
+Singewing.prototype.register = function () {
+  this.name = $("#name").val();
+  this.socket.emit("register", this.name);
+}
+
+//TX - Get/Update user list
+Singewing.prototype.updateUserList = function () {
+  this.socket.emit("updateUserList");
+};
+
+var singewing = new Singewing();
+
+//singewing.test();
+singewing.ping();
+
+$(function () {
+  $("#connect").click(function () {
+      singewing.register();
+  });
+});
---
+++ server.js
@@ -0,0 +1,117 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var users = [];
+
+var log = function (msg){
+  var date = new Date();
+  console.log("[" + date.toLocaleDateString('en-GB') + " - " +date.toLocaleTimeString('en-GB') + "] " + msg);
+};
+
+var getRandomInt = function(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+var validateUser = function(user) {
+  for (var i=0; i<users.length; i++)
+  {
+    if(users[i]["name"] == user["name"] || users[i]["socketId"] == user["socketId"])
+    {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+var findUser = function (socketId) {
+  for (var i=0; i<users.length; i++)
+  {
+    if(users[i]["socketId"] == socketId)
+    {
+      return i;
+    }
+  }
+
+  return null;
+}
+
+//HTTP Service
+app.use(express.static('js'));
+app.use(express.static('css'));
+app.use(express.static('images'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+//Socket Service
+io.on('connection', function(socket){
+  log("User connected");
+  socket.emit('connection');
+
+  //test
+  socket.on('test', function(){
+    log("Test request received... Emitting ack.");
+    socket.emit('ack');
+  });
+
+  //ping
+  socket.on('sw_ping', function (time){
+    var now = new Date().getTime();
+    socket.emit('sw_pong', now);
+    log("ping received, sent pong ==> time diff: " + (now - time) + "ms");
+  });
+
+  //register
+  socket.on('register', function (name) {
+    log("Received register request from " + name);
+    var color = [getRandomInt(0, 255), getRandomInt(0, 255), getRandomInt(0, 255)];
+    var user = {"name": name, "color": color, "socketId": socket.id};
+
+    log("name: " + user["name"] + "   ==>   color:" + user["color"] + "   ==>   socket:" + user["socketId"]);
+
+    if(validateUser(user))
+    {
+      log("User validated");
+      users.push(user);
+      io.emit("newUser", user);
+      log(users.length + " Users online");
+    }
+    else
+    {
+      log("user already exists.");
+      socket.emit("registrationError", "User already registered.");
+    }
+  });
+
+  //Update user list
+  socket.on("updateUserList", function (){
+    socket.emit("updateUserList", users);
+  });
+
+  //Disconnect
+  socket.on('disconnect', function(){
+    log('User disconnected');
+    var index = findUser(socket.id);
+    if(index != null)
+    {
+      log("removing user " + users[index]["name"]);
+      users.splice(index, 1);
+      log(users.length + " users left.");
+    }
+    else
+    {
+      log("user not found. just disconencted without registering. This is not necessarily a problem.");
+    }
+  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});

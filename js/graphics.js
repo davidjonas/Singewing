aa4b1d53c8c10eed657e27a7fd4a0ff2d075ef67
David Jonas <davidjonasdesign@gmail.com>
2017-11-20 15:16:58 +0100

    Small mobile design adaptation.

--- js/graphics.js
+++ js/graphics.js
@@ -26,21 +26,40 @@ function draw()
 
     noStroke();
     fill(singewing.users[i]["color"][0],singewing.users[i]["color"][1],singewing.users[i]["color"][2], 30);
-    ellipse(xPos,yPos,width*0.1 + pulse,width*0.1 + pulse);
-    ellipse(xPos,yPos,width*0.08 + pulse,width*0.08 + pulse);
-    ellipse(xPos,yPos,width*0.06 + pulse,width*0.06 + pulse);
-    ellipse(xPos,yPos,width*0.02 + pulse,width*0.02 + pulse);
+
+    if(width > 600)
+    {
+      ellipse(xPos,yPos,width*0.1 + pulse,width*0.1 + pulse);
+      ellipse(xPos,yPos,width*0.08 + pulse,width*0.08 + pulse);
+      ellipse(xPos,yPos,width*0.06 + pulse,width*0.06 + pulse);
+      ellipse(xPos,yPos,width*0.02 + pulse,width*0.02 + pulse);
+
+      if(singewing.users[i]["name"] == singewing.name)
+      {
+        noFill();
+        stroke(singewing.users[i]["color"][0],singewing.users[i]["color"][1],singewing.users[i]["color"][2], 100);
+        strokeWeight(5);
+        ellipse(xPos,yPos,width*0.1,width*0.1);
+      }
+    }
+    else {
+      ellipse(xPos,yPos,width*0.35 + pulse,width*0.35 + pulse);
+      ellipse(xPos,yPos,width*0.25 + pulse,width*0.25 + pulse);
+      ellipse(xPos,yPos,width*0.10 + pulse,width*0.10 + pulse);
+      ellipse(xPos,yPos,width*0.05 + pulse,width*0.05 + pulse);
+
+      if(singewing.users[i]["name"] == singewing.name)
+      {
+        noFill();
+        stroke(singewing.users[i]["color"][0],singewing.users[i]["color"][1],singewing.users[i]["color"][2], 100);
+        strokeWeight(5);
+        ellipse(xPos,yPos,width*0.3,width*0.3);
+      }
+    }
 
     fill(255);
+    noStroke();
     text(singewing.users[i]["name"],xPos, yPos);
-
-    if(singewing.users[i]["name"] == singewing.name)
-    {
-      noFill();
-      stroke(singewing.users[i]["color"][0],singewing.users[i]["color"][1],singewing.users[i]["color"][2], 100);
-      strokeWeight(5);
-      ellipse(xPos,yPos,width*0.1,width*0.1);
-    }
   }
 }
 
